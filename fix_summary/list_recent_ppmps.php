92836ce027db1a8ee5017f44616d9d8a56bfba98
jmhumbid <jmhumbid@gmail.com>
2026-05-22 23:15:07 +0800

    Delete fix_summary directory

--- fix_summary/list_recent_ppmps.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config/database.php';
-$db = getDB();
-$stmt = $db->query("SELECT id, ppmp_number, department_id, fiscal_year, status FROM ppmp ORDER BY id DESC LIMIT 10");
-echo "Recent PPMPs:\n";
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo "  PPMP #{$row['id']} - {$row['ppmp_number']} - Dept {$row['department_id']} - {$row['fiscal_year']} - {$row['status']}\n";
-}
-?>
